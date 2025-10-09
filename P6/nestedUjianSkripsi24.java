9a61db91a4e9d3fc298eb3cea2d561890dbf4914
borzooraa <nurfakiyahr24@gmail.com>
2025-10-09 11:19:06 +0700

    P6_Percobaan3

---
+++ P6/nestedUjianSkripsi24.java
@@ -0,0 +1,31 @@
+package P6;
+
+import java.util.Scanner;
+
+public class nestedUjianSkripsi24 {
+    public static void main(String[] args) {
+        Scanner Kiyah = new Scanner (System.in);
+        String pesan;
+        System.out.print("Apakah mahasiswa sudah bebas kompe? (Ya/Tidak): ");
+        String bebasKompen = Kiyah.nextLine();
+
+        System.out.print("Masukkan jumlah log bimbingan Pembimbing 1: ");
+        int bimbinganP1 = Kiyah.nextInt();
+        System.out.print("Masukkan jumlah log bimbingan Pembimbing 2: ");
+        int bimbinganP2 = Kiyah.nextInt();
+
+        if (bebasKompen.equalsIgnoreCase("Ya")){
+            if (bimbinganP1 > 8 && bimbinganP2 > 4){
+                System.out.println("Semua syarat terpenuhi. Mahasiswa boleh mendaftar ujian skripsi");
+            }else if (bimbinganP1 < 8 && bimbinganP2 < 4){
+                System.out.println("Gagal! Log pembimbing P1 kurang dari 8 kai dan P2 kurang dari 4 kali");
+            }else if (bimbinganP1 < 8){
+                System.out.println("Gagal! Log pembimbing P1 belum mencapai 8 kali");
+            } else {
+                System.out.println("Gagal! Log bimbingan P2 belum mencapai 4 kali");
+            }
+     }  else{
+        System.out.println("Gagal! Mahasiswa masih memiliki tanggungan kompen");
+     }
+    }
+}
