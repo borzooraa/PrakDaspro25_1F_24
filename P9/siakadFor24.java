ae689364df9684f1fdc25f8da86e303cf57b5038
borzooraa <nurfakiyahr24@gmail.com>
2025-10-23 12:12:44 +0700

    P9_Percobaan1_Pertanyaan4

--- P9/siakadFor24.java
+++ P9/siakadFor24.java
@@ -5,10 +5,11 @@ import java.util.Scanner;
 public class siakadFor24 {
     public static void main(String[] args) {
         Scanner Kiyah = new Scanner (System.in);
+        int cLulus=0, cTdkLulus=0;
         double nilai, nTinggi=0, nRendah=100;
 
         for (int i = 1; i <=10; i++) {
-            System.out.print("Masukkan nilai mahasiswa ke-"+i+":");
+            System.out.print("Masukkan nilai mahasiswa ke-"+i+": ");
             nilai = Kiyah.nextDouble();
             if (nilai > nTinggi){
                 nTinggi = nilai;
@@ -16,8 +17,15 @@ public class siakadFor24 {
             if (nilai < nRendah){
                 nRendah = nilai;
             }
+            if (nilai >= 60) {
+                cLulus++;
+            }else
+             cTdkLulus++; 
         }
+        
         System.out.print("Nilai tertinggi: "+nTinggi);
         System.out.print("\nNilai terendah: "+nRendah);
+        System.out.print("\nJumlah mahasiswa yang lulus: "+cLulus);
+        System.out.print("\nJumlah mahasiswa yang tidak lulus: "+cTdkLulus);
     }
 }
