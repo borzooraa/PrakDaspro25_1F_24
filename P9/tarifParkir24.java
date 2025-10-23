8743535b5ad96ca4a17236166263a66d422e22a3
borzooraa <nurfakiyahr24@gmail.com>
2025-10-23 23:25:20 +0700

    P9_Tugas2

--- P9/tarifParkir24.java
+++ P9/tarifParkir24.java
@@ -4,9 +4,33 @@ import java.util.Scanner;
 
 public class tarifParkir24 {
     public static void main(String[] args) {
-        Scanner Kiyah = new Scanner (System.in);
-        int durasi=0, total=0, harga, jenis;
-           
-         
+        Scanner Kiyah = new Scanner(System.in);
+        int durasi = 0, total = 0, jenis;
+
+        while (true) {
+            System.out.print("\nMasukkan jenis kendaraan (1 Mobil, 2 Motor, 0 Keluar): ");
+            jenis = Kiyah.nextInt();
+
+            if (jenis == 0) {
+                break;
+            }
+
+            if (jenis == 1 || jenis == 2) {
+                System.out.print("Masukkan durasi parkir: ");
+                durasi = Kiyah.nextInt();
+                if (durasi > 5) {
+                    total += 12500;
+                } else {
+                    if (jenis == 1) {
+                        total += durasi * 3000;
+                    } else {
+                        total += durasi * 2000;
+                    }
+                }
+            } else {
+                System.out.print("Jenis kendaraan tidak valid, masukkan ulang jenis kendaraan!");
+                continue;
+            }
+        } System.out.print("Total pendapatan hari ini: Rp" + total);
     }
 }
