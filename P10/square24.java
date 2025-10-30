d05eae5ab33d5a1145a2b239c72195c1b4620842
borzooraa <nurfakiyahr24@gmail.com>
2025-10-30 09:13:04 +0700

    P10_Percobaan2

---
+++ P10/square24.java
@@ -0,0 +1,17 @@
+package P10;
+
+import java.util.Scanner;
+
+public class square24 {
+    public static void main(String[] args) {
+        Scanner Kiyah = new Scanner(System.in);
+        System.out.print("Masukkan nilai n = ");
+        int n = Kiyah.nextInt();
+        for (int iOuter = 0; iOuter <= n; iOuter++) {
+            for (int i = 1; i<=n; i++) {
+                System.out.print("*");  
+            }
+            System.out.println();    
+        }
+    }
+}
