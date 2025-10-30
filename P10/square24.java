7ac4103d69a945e6740d59fa75830f4204011b53
borzooraa <nurfakiyahr24@gmail.com>
2025-10-30 09:45:26 +0700

    P10_Percobaan2_Pertanyaan1

--- P10/square24.java
+++ P10/square24.java
@@ -7,7 +7,7 @@ public class square24 {
         Scanner Kiyah = new Scanner(System.in);
         System.out.print("Masukkan nilai n = ");
         int n = Kiyah.nextInt();
-        for (int iOuter = 1; iOuter <= n; iOuter++) {
+        for (int iOuter = 0; iOuter <= n; iOuter++) {
             for (int i = 1; i<=n; i++) {
                 System.out.print("*");  
             }
