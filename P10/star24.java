cc9dccfb4c0651fbec4cc256d34df229b9399d0f
borzooraa <nurfakiyahr24@gmail.com>
2025-10-30 09:12:30 +0700

    P10_Percobaan1

---
+++ P10/star24.java
@@ -0,0 +1,16 @@
+package P10;
+
+import java.util.Scanner;
+
+public class star24 {
+    public static void main(String[] args) {
+        Scanner Kiyah = new Scanner(System.in);
+        System.out.print("Masukkan nilai n = ");
+        int n = Kiyah.nextInt();
+
+        for (int i = 1; i <= n; i+=2) {
+            System.out.print("*");
+            
+        }
+    }
+}
