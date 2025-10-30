778402e55a78ebb3034d8fd2e71c8821e9ec36f6
borzooraa <nurfakiyahr24@gmail.com>
2025-10-30 10:34:00 +0700

    P10_Percobaan3

---
+++ P10/triangle24.java
@@ -0,0 +1,21 @@
+package P10;
+
+import java.util.Scanner;
+
+public class triangle24 {
+    public static void main(String[] args) {
+        Scanner Kiyah = new Scanner (System.in);
+        System.out.print("Masukkan nilai n = ");
+        int n = Kiyah.nextInt();
+        int i = 0;
+
+        while (i<=n) {
+            int j = 0;
+            while (j < i){
+                System.out.print("*");
+                j++;
+            }
+            i++;
+        }
+    }
+}
