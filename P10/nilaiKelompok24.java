57442376c517c18a6398d95921aaf2536db8f6ab
borzooraa <nurfakiyahr24@gmail.com>
2025-11-05 22:03:55 +0700

    P10_Percobaan4_Pertanyaan2

--- P10/nilaiKelompok24.java
+++ P10/nilaiKelompok24.java
@@ -5,21 +5,28 @@ import java.util.Scanner;
 public class nilaiKelompok24 {
     public static void main(String[] args) {
         Scanner Kiyah = new Scanner(System.in);
-        int   i=1, nilai;
-        float totNilai, rataNilai;
-
+        int   i=1, nilai, kelTinggi=0;
+        float totNilai, rataNilai, rataTinggi=0;
+     
         while (i<=6) {
-            System.out.println("Kelompok "+i);
+            System.out.println("\nKelompok "+i);
             
-                totNilai=0;
+            
+            totNilai=0;
                     for (int j = 1; j <= 5; j++) {
                         System.out.print("Nilai dari Kelompok Penilai" +j+": ");
                         nilai = Kiyah.nextInt();
                         totNilai+=nilai;
                     }
                     rataNilai=totNilai/5;
-                    System.out.println("Kelompok "+i+ ": nilai rata-rata " +rataNilai);
+                    if (rataNilai>rataTinggi){
+                        rataTinggi = rataNilai;
+                        kelTinggi=i;
+                    }
+                    System.out.print("Kelompok "+i+ ": nilai rata-rata " +rataNilai);
                     i++;
-        }
+                }
+                System.out.print("\nNilai rata-rata tertinggi: "+ rataTinggi);
+                System.out.print("\nKelompok dengan nilai rata-rata tertinggi: "+kelTinggi);
     }
 }
