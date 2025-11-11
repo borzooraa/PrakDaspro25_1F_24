5353436dd1a44513ee7dff2704ca0674f439ff1c
borzooraa <nurfakiyahr24@gmail.com>
2025-11-11 18:36:09 +0700

    P11_Percobaan3_Pertanyaan2

--- P11/arrayRataNilai24.java
+++ P11/arrayRataNilai24.java
@@ -5,21 +5,30 @@ import java.util.Scanner;
 public class arrayRataNilai24 {
     public static void main(String[] args) {
         Scanner Kiyah = new Scanner(System.in);
-        int[] nilaiMhs = new int[10]; 
-        int totMhsLul=0;
-        double total = 0, rata = 0;
-
+        double totLulus = 0, totTdkLul=0, rataLulus, rataTdkLulus;
+        int jlhLulus = 0, jlhTdkLulus= 0;
+        
+        System.out.print("Masukkan jumlah mahasiswa : ");
+        int jlhMhs = Kiyah.nextInt();
+        int[] nilaiMhs = new int[jlhMhs]; 
+        
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+ " : ");
             nilaiMhs[i]= Kiyah.nextInt();
-          } for (int i = 0; i < nilaiMhs.length; i++) {
-            total += nilaiMhs[i];
-            if (nilaiMhs[i] > 70) {
-                totMhsLul++;
-            }
-        } rata = total/nilaiMhs.length;
-        System.out.println("Rata-rata nilai = "+rata);
-        System.out.println("Jumlah mahasiswa lulus = "+totMhsLul);
+           if (nilaiMhs[i] > 70) {
+            totLulus += nilaiMhs[i];
+            jlhLulus++;
+          }else {
+            totTdkLul += nilaiMhs[i];
+            jlhTdkLulus++;
+          }
+        }
+        
+        rataLulus=(jlhLulus > 0)? totLulus/jlhLulus:0;
+        rataTdkLulus=(jlhTdkLulus > 0)? totTdkLul/jlhTdkLulus:0;
 
+        System.out.println("Rata-rata nilai lulus = "+rataLulus);
+        System.out.println("Rata-rata nilai tidak lulus = "+rataTdkLulus);
+        
     }
 }
