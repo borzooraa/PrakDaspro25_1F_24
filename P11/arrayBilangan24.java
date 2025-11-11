42af3abed5fb4283b69f275618feda89d29a072c
borzooraa <nurfakiyahr24@gmail.com>
2025-11-11 16:28:37 +0700

    P11_Percobaan1_Pertanyaan3

--- P11/arrayBilangan24.java
+++ P11/arrayBilangan24.java
@@ -3,13 +3,10 @@ package P11;
 public class arrayBilangan24 {
     public static void main(String[] args) {
         int[] bil = {
-            5, 13, -7, 17
+                5, 13, -7, 17
         };
-
-
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
+        for (int i = 0; i < 4; i++) {
+            System.out.println(bil[i]);
+        }
     }
 }
