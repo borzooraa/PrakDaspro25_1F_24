a633295378f7cd69ca5b780acdd3640867513baa
borzooraa <nurfakiyahr24@gmail.com>
2025-11-11 19:13:27 +0700

    P11_Percobaan4_Pertanyaan3

--- P11/searchNilai24.java
+++ P11/searchNilai24.java
@@ -10,29 +10,28 @@ public class searchNilai24 {
         int jumlah = Kiyah.nextInt();
         int hasil = -1;
         int[] arrNilai = new int[jumlah];
-        
+
         for (int i = 0; i < jumlah; i++) {
-            System.out.print("Masukkan nilai mahsiswa ke-"+(i+1)+": ");
-            arrNilai[i]=Kiyah.nextInt();
+            System.out.print("Masukkan nilai mahsiswa ke-" + (i + 1) + ": ");
+            arrNilai[i] = Kiyah.nextInt();
         }
 
         System.out.print("Masukkan nilai yang ingin dicari: ");
         int key = Kiyah.nextInt();
 
-
         for (int i = 0; i < arrNilai.length; i++) {
             if (key == arrNilai[i]) {
-                hasil=i;
+                hasil = i;
                 break;
             }
         }
         System.out.println();
-         if (hasil != -1) {
-             System.out.println("Nilai "+key+" ketemu, merupakan nilai mahasiswa ke-"+(hasil+1));
-            
-         }else {
+        if (hasil != -1) {
+            System.out.println("Nilai " + key + " ketemu, merupakan nilai mahasiswa ke-" + (hasil + 1));
 
-             System.out.println("Nilai"+key+" tidak ditemukan dalam data.");
-         }
+        } else {
+
+            System.out.println("Nilai yang dicari tidak ditemukan");
+        }
     }
 }
