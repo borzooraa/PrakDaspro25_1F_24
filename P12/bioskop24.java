1906798111a2332efca3f134951d5cd638752066
borzooraa <nurfakiyahr24@gmail.com>
2025-11-18 09:26:35 +0700

    P12_Percobaan1_Pertanyaan8

--- P12/bioskop24.java
+++ P12/bioskop24.java
@@ -19,14 +19,14 @@ public class bioskop24 {
         System.out.printf("%s \t %s \n", penonton[3][0], penonton[3][1]);
 
         System.out.println(penonton.length);
-        
+
         for (String[] barisPenonton : penonton) {
-            System.out.println("Panjang baris: "+barisPenonton.length);
+            System.out.println("Panjang baris: " + barisPenonton.length);
         }
         System.out.println("Penonton pada baris ke-3: ");
 
-        for (int i = 0; i < penonton[2].length; i++) {
-            System.out.println(penonton[2][i]);
+        for (String i : penonton[2]) {
+            System.out.println(i);
         }
     }
 }
