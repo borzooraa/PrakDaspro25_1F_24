c5f2a72cfc2dda316f10ee19ec052148670ba31d
borzooraa <nurfakiyahr24@gmail.com>
2025-11-18 14:09:40 +0700

    P12_Percobaan4_Modifikasi2

--- P12/SIAKAD24.java
+++ P12/SIAKAD24.java
@@ -9,10 +9,14 @@ public class SIAKAD24 {
 
         for (int i = 0; i < nilai.length; i++) {
             System.out.print("\ninput nilai mahasiswa ke-"+(i+1)+"\n");
+            double totNilSiswa=0;
+
             for (int j = 0; j < nilai[i].length; j++) {
                 System.out.print("Nilai mata kuliah "+(j+1)+": ");
                 nilai [i][j] = Kiyah.nextInt();
+                totNilSiswa+=nilai[i][j];
             }
+            System.out.println("Nilai rata-rata: "+ totNilSiswa/3);
         }
     }
 }
