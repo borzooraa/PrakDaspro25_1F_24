31f11fc1069581a7edbee3a82cf35c57f518047f
borzooraa <nurfakiyahr24@gmail.com>
2025-11-18 11:39:16 +0700

    P12_Percobaan2__Pertanyaan2

--- P12/bioskopWithScanner24.java
+++ P12/bioskopWithScanner24.java
@@ -6,26 +6,41 @@ public class bioskopWithScanner24 {
     public static void main(String[] args) {
         Scanner Kiyah = new Scanner(System.in);
         int baris, kolom;
-        String nama, next;
+        String nama, pilih;
 
         String[][] penonton = new String[4][2];
 
         while (true) {
-            System.out.print("Masukkan nama: ");
-            nama=Kiyah.nextLine();
-            System.out.print("Masukkan baris: ");
-            baris= Kiyah.nextInt();
-            System.out.print("Masukkan kolom: ");
-            kolom= Kiyah.nextInt();
-            Kiyah.nextLine();
+            System.out.println("\n=== MENU BIOSKOP ===");
+            System.out.println("1. Input data penonton \n2. Tampilkan daftar penonton \n3. Exit");
+            System.out.print("Pilih menu (1/2/3): ");
+            pilih = Kiyah.nextLine();
 
-            penonton[baris-1][kolom-1]=nama;
-
-            System.out.print("Input penonton lainnya? (y/n): ");
-            next = Kiyah.nextLine();
-
-            if (next.equalsIgnoreCase("n")) {
+            if (pilih.equals("1")) {
+                System.out.print("Masukkan nama: ");
+                nama=Kiyah.nextLine();
+                System.out.print("Masukkan baris (1-4): ");
+                baris= Kiyah.nextInt();
+                System.out.print("Masukkan kolom (1-2): ");
+                kolom= Kiyah.nextInt();
+                Kiyah.nextLine();
+    
+                penonton[baris-1][kolom-1]=nama;
+                System.out.println("Data tersimpan");
+            }else if (pilih.equals("2")){
+                System.out.println("\n=== DAFTAR PENONTON ===");
+                for (int i = 0; i < penonton.length; i++) {
+                    System.out.print("Baris ke-"+(i+1)+ ": ");
+                    for (int j = 0; j < penonton[i].length; j++) {
+                        System.out.print(penonton[i][j]+ (j == penonton[i].length - 1 ? "" : ", "));
+                    }
+                    Kiyah.nextLine();
+                }
+            }else if (pilih.equals("3")) {
+                System.out.println("Keluar dari menu");
                 break;
+            }else {
+                System.out.println("Menu tidak valid");
             }
         }
     }
