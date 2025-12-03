a745f54722862dcce3c75b117139d93b2a25ac3b
borzooraa <nurfakiyahr24@gmail.com>
2025-12-03 15:56:58 +0700

    P14_Percobaan3

--- kafe24.java
+++ kafe24.java
@@ -1,12 +1,17 @@
-public class kafe24 {
-    public static void Menu(String namaPel, boolean isMem, String kodePromo){
-        System.out.println("Selamat datang, "+namaPel+"!");
+import java.util.Scanner;
 
-        if (isMem){
+public class kafe24 {
+    static Scanner Kiyah = new Scanner(System.in);
+
+    public static void Menu(String namaPel, boolean isMem, String kodePromo) {
+        System.out.println("Selamat datang, " + namaPel + "!");
+
+        if (isMem) {
             System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
-        } if (kodePromo.equalsIgnoreCase("DISKON50")){
+        }
+        if (kodePromo.equalsIgnoreCase("DISKON50")) {
             System.out.println("Selamat, anda mendapatkan diskon 50%!");
-        }else if (kodePromo.equalsIgnoreCase("Diskon30")) {
+        } else if (kodePromo.equalsIgnoreCase("Diskon30")) {
             System.out.println("Selamat, anda mendapatkan diskon 30%!");
         }
 
@@ -18,9 +23,23 @@ public class kafe24 {
         System.out.println("5. Roti Bakar - Rp 10.000");
         System.out.println("6. Mie Goreng - Rp 18.000");
         System.out.println("===========================");
-        System.out.println("Silahkan pilih menu yang anda inginkan");
-    } public static void main(String[] args) {
-        Menu("Andi", true,"diskon50");
-        
+    }
+
+    public static int hitungTotHrg24(int pilihanMenu, int banyakItem) {
+        int[] hrgItems = { 15000, 20000, 22000, 12000, 10000, 18000 };
+
+        int hargaTotal = hrgItems[pilihanMenu - 1] * banyakItem;
+        return hargaTotal;
+    }
+
+    public static void main(String[] args) {
+        Menu("Andi", true, "diskon50");
+        int pilMenu, totItems, totalBayar;
+        System.out.print("Silahkan pilih menu yang anda inginkan: ");
+        pilMenu = Kiyah.nextInt();
+        System.out.print("Jumlah item: ");
+        totItems = Kiyah.nextInt();
+        totalBayar = hitungTotHrg24(pilMenu, totItems);
+        System.out.println("Total yang harus dibayarkan: "+totalBayar);
     }
 }
