611fd91c6e1c52c68df88fc2aa73306dd6968fea
borzooraa <nurfakiyahr24@gmail.com>
2025-12-03 17:49:04 +0700

    P14_Percobaan4_Pertanyaan2

--- pengunjungCafe24.java
+++ pengunjungCafe24.java
@@ -1,8 +1,8 @@
 public class pengunjungCafe24 {
     static void daftarPengunjung(String... namaPengunjung) {
         System.out.println("Daftar Nama Pengunjung: ");
-        for (int i = 0; i < namaPengunjung.length; i++) {
-            System.out.println("- " + namaPengunjung[i]);
+        for (String nama : namaPengunjung) {
+            System.out.println("- "+nama);
         }
     }
 
